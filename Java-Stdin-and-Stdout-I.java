48b40ed5f7c517922595d89dac4a196dd8f9c3d0
tarunkumarreddytirupathi <150784964+tarunkumarreddytirupathi@users.noreply.github.com>
2024-07-30 17:17:10 +0530

    create 3 files

---
+++ Java-Stdin-and-Stdout-I.java
@@ -0,0 +1,25 @@
+/* 
+
+
+
+
+copy the below code and execute in HackerRank
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int firstNumber=scanner.nextInt();
+        int secondNumber=scanner.nextInt();
+        int threeNumber=scanner.nextInt();
+        
+        System.out.println(firstNumber+"\n"+secondNumber+"\n"+threeNumber);
+        scanner.close();
+    }
+}
+
+
+
+*/
---
+++ Welcome-to-Java.java
@@ -0,0 +1,17 @@
+/* 
+
+
+
+
+copy the below code and execute in HackerRank
+
+public class Solution {
+
+    public static void main(String[] args) {
+      System.out.println("Hello, World.\nHello, Java.");
+    }
+}
+
+
+
+*/
