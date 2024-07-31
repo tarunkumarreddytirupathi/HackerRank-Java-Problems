41aee2a0d9df3955828ccc84d9cd0d8db0b2b36a
tarunkumarreddytirupathi <150784964+tarunkumarreddytirupathi@users.noreply.github.com>
2024-07-31 19:25:19 +0530

    Update Java-If-Else.java

--- Java-If-Else.java
+++ Java-If-Else.java
@@ -6,12 +6,7 @@
 copy the below code and execute in HackerRank
 
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+
 
 public class Solution {
 
