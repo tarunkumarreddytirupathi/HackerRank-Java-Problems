bb4fa678f462c8c97a98b47fdc4b5c0b81ae12ee
tarunkumarreddytirupathi <150784964+tarunkumarreddytirupathi@users.noreply.github.com>
2024-08-01 14:10:43 +0530

    Update Java-Stdin-and-Stdout-II.java

--- Java-Stdin-and-Stdout-II.java
+++ Java-Stdin-and-Stdout-II.java
@@ -5,7 +5,25 @@
 
 copy the below code and execute in HackerRank
 
+import java.util.Scanner;
 
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        double d = scanner.nextDouble();
+        scanner.nextLine();
+        String s=scanner.nextLine();
+
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+        scanner.close();
+    }
+}
 
 
 */
