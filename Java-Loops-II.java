eb166c5c9e5c56c568a0ae810e05367f2f54f855
tarunkumarreddytirupathi <150784964+tarunkumarreddytirupathi@users.noreply.github.com>
2024-08-05 00:13:16 +0530

    Update Java-Loops-II.java

--- Java-Loops-II.java
+++ Java-Loops-II.java
@@ -5,7 +5,35 @@
 
 copy the below code and execute in HackerRank
 
+import java.util.*;
+import java.io.*;
 
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            long sum=a+b;
+            System.out.print(sum);
+                   if (n>1) {
+                       System.out.print(" ");
+                        for(int j=1;j<n;j++){
+            sum+=(Math.pow(2, j))*(b);
+                    if(j==n-1){
+                
+                     System.out.print(sum);}
+                     else{
+                    System.out.print(sum+" ");}
+            
+            }
+                   }System.out.println();
+        }
+        in.close();
+    }
+}
 
 
 */
