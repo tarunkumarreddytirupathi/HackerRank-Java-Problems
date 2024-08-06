260127861a8e4607ab6fd7c5804b416487b2fd21
tarunkumarreddytirupathi <150784964+tarunkumarreddytirupathi@users.noreply.github.com>
2024-08-07 02:47:25 +0530

    Update Java-Static-Initializer-Block.java

--- Java-Static-Initializer-Block.java
+++ Java-Static-Initializer-Block.java
@@ -6,6 +6,27 @@
 copy the below code and execute in HackerRank
 
 
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //scanner class
+        Scanner s=new Scanner(System.in);
+        
+        // input breadhth integer
+        int b=s.nextInt();
+        // input heigth integer
+        int h=s.nextInt();
+        if((b<=0) || (h<=0))
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        else
+        System.out.println(b * h);
+        s.close();
+        
+}
+}
 
 
 
