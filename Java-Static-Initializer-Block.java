3cd9d95ac31c567a1c4ee54dddfec20364a36fed
tarunkumarreddytirupathi <150784964+tarunkumarreddytirupathi@users.noreply.github.com>
2024-08-07 02:48:03 +0530

    Update Java-Static-Initializer-Block.java

--- Java-Static-Initializer-Block.java
+++ Java-Static-Initializer-Block.java
@@ -21,8 +21,9 @@ public class Solution {
         int h=s.nextInt();
         if((b<=0) || (h<=0)){
             System.out.println("java.lang.Exception: Breadth and height must be positive");}
-        else
-        System.out.println(b * h);
+        else{
+        System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
         s.close();
         
 }
