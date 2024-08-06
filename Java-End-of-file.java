e93d9a943bf24d05edc57326ad5a75f9fff95542
tarunkumarreddytirupathi <78693308+krishnakarthik123427@users.noreply.github.com>
2024-08-06 19:09:48 +0530

    Update Java-End-of-file.java

--- Java-End-of-file.java
+++ Java-End-of-file.java
@@ -6,6 +6,21 @@
 copy the below code and execute in HackerRank
 
 
+import java.io.*;
+import java.util.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int count=0;
+        while (s.hasNext()) {
+            String line = s.nextLine();
+            ++count;
+            System.out.println(count+" "+line);
+        }
+        s.close();
+    }
+}
 
 */
