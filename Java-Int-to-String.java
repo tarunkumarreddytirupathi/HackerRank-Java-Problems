f578d89c5ab442d1b481480313b63544fca1e797
tarunkumarreddytirupathi <150784964+tarunkumarreddytirupathi@users.noreply.github.com>
2024-08-08 04:31:14 +0530

    Update Java-Int-to-String.java

--- Java-Int-to-String.java
+++ Java-Int-to-String.java
@@ -6,7 +6,25 @@
 copy the below code and execute in HackerRank
 
 
+import java.io.*;
+import java.util.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+        //scanner class
+        Scanner s=new Scanner(System.in);
+        // taking integer input
+        int number=s.nextInt();
+        if(number>=-100&&number<=100){
+            System.out.println("Good job");
+        }else{
+            System.out.println("Wrong answer");
+        }
+        //close the scanner class
+        s.close();
+    }
+}
 
 
 */
