dbc5b9d466b385d074d15589a7f4ae26316133de
tarunkumarreddytirupathi <150784964+tarunkumarreddytirupathi@users.noreply.github.com>
2024-08-12 10:18:49 +0530

    Update Java-Substring.java

--- Java-Substring.java
+++ Java-Substring.java
@@ -2,6 +2,24 @@
 
 copy the below code and execute in HackerRank
 
+mport java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        in.close();
+        for (int i = start; i < end; i++) {
+            System.out.print(S.charAt(i));
+        }
+    }
+}
 
 */
