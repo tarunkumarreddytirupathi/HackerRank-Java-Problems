6b37fa13b1876a3a23322419abb16010d710f9a5
tarunkumarreddytirupathi <150784964+tarunkumarreddytirupathi@users.noreply.github.com>
2024-08-14 08:38:33 +0530

    Update Java-String-Reverse.java

--- Java-String-Reverse.java
+++ Java-String-Reverse.java
@@ -5,6 +5,28 @@
 
 copy the below code and execute in HackerRank
 
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        sc.close();
+         StringBuilder sb = new StringBuilder(A);
+        String B = sb.reverse().toString();
+        if (A.equals(B)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
+
+
+
 
 
 
