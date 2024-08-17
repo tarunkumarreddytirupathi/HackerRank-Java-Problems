330521516f934d4b2d8e27e687b2fac0b330bb0b
tarun <tarunkumarreddytirupathi@gmail.com>
2024-08-17 17:14:04 +0530

    update file

--- Pattern-Syntax-Checker.java
+++ Pattern-Syntax-Checker.java
@@ -6,6 +6,34 @@
 copy the below code and execute in HackerRank
 
 
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class Solution
+{
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		while(testCases>0){
+			String pattern = in.nextLine();
+          	try {
+            Pattern.compile(pattern);
+            System.out.println("Valid");
+            }    
+            catch (Exception e) {
+            System.out.println("Invalid");
+            
+              
+        }
+        --testCases;
+        
+		}
+        in.close();
+	}
+}
+
+
+
 
 
 */
