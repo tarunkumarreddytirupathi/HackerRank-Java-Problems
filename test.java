fd95d614400f99d05ec1177cfdb56478a8e7a2f4
tarun kumar reddy tirupathi <tarunkumarreddytirupathi@gmail.com>
2024-10-12 00:57:11 +0530

    Add Java class with main method for condition check.

---
+++ test.java
@@ -0,0 +1,14 @@
+/**
+ * test
+ */
+public class test {
+
+    public static void main(String[] args) {
+        String s="Asdad";
+        String s1="Asdad";
+        char s2='1';
+        if(s2!='0'){
+            System.out.println("0");
+        }
+    }
+}
